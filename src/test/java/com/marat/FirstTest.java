8e11ec9a89ba0716e0dfdaf8176784b21c77fc3f
marat <maratussr@gmail.com>
2023-01-07 20:13:34 +0400

    added new file firstTest

---
+++ src/test/java/com/marat/FirstTest.java
@@ -0,0 +1,12 @@
+package com.marat;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    void firstTest() {
+        Assertions.assertTrue(true);
+    }
+}
